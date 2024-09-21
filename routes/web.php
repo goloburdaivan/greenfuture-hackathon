1edd1fc7b6b69df3fdb39cf96b94a7a8aa16fa0a
goloburdaivan <goloburda.ivan@lll.kpi.ua>
2024-09-21 14:57:00 +0300

    Remove unused middleware

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,19 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(AuthController::class)->group(function() {
     Route::get('/login', 'indexLogin')->name('login.index');
-    Route::post('/login', 'login')->name('login')->withoutMiddleware(VerifyCsrfToken::class);
+    Route::post('/login', 'login')->name('login');
     Route::get('/register', 'indexRegister')->name('register.index');
-    Route::post('/register', 'register')->name('register')->withoutMiddleware(VerifyCsrfToken::class);
+    Route::post('/register', 'register')->name('register');
 });
 
 Route::get('/', function() {
     return response()->json([
         'message' => 'test',
     ]);
-})->middleware('auth');
+})
+    ->name('user.home')
+    ->middleware('auth');
