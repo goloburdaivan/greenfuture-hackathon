18c3fc5a36752c108fad78b2fd193c585cb1fc6f
Mykola Volokyta <nikv2005@gmail.com>
2024-09-22 09:37:11 +0300

    Added school form connection with BE

--- resources/js/Pages/School/Show.jsx
+++ resources/js/Pages/School/Show.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from "@/Components/Header/Header.jsx";
 import School from "@/Components/School/School.jsx";
+import Footer from "@/Components/Footer/Footer.jsx";
 
 export default function Show({ school }) {
     return (
         <>
             <Header/>
             <School school={school}/>
+            <Footer/>
         </>
     );
 }
